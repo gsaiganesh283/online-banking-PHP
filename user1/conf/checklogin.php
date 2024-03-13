c7aed426ca7ae9ceff450b37f5ad71d7d29afd7a
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2024-03-13 22:27:27 +0530

    changed staff to user

--- user1/conf/checklogin.php
+++ user1/conf/checklogin.php
@@ -1,12 +1,12 @@
 <?php
 function check_login()
 {
-if(strlen($_SESSION['staff_id'])==0)
+if(strlen($_SESSION['account_id'])==0)
 	{
 		$host = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra="pages_staff_index.php";
-		$_SESSION["staff_id"]="";
+		$extra="pages_user_index.php";
+		$_SESSION["account_id"]="";
 		header("Location: http://$host$uri/$extra");
 	}
 }
