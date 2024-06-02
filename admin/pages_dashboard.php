dfdc62d4e80e60e37c0a21be8ddd33bf7c46de22
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2024-06-02 21:20:23 +0530

    client is changed to branch's in admin dashboard

--- admin/pages_dashboard.php
+++ admin/pages_dashboard.php
@@ -152,7 +152,7 @@ $stmt->close();
               <div class="info-box">
                 <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
                 <div class="info-box-content">
-                  <span class="info-box-text">Clients</span>
+                  <span class="info-box-text">Branchs</span>
                   <span class="info-box-number">
                     <?php echo $iBClients; ?>
                   </span>
