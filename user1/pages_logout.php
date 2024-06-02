9b00882cdf6c00a2ac4af27e69057251012af964
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2024-06-02 21:21:21 +0530

    code is updated when logout is to redirect to the user index page in user

--- user1/pages_logout.php
+++ user1/pages_logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    unset($_SESSION['staff_id']);
+    unset($_SESSION['account_id']);
     session_destroy();
 
-    header("Location: pages_staff_index.php");
+    header("Location: pages_user_index.php");
     exit;
