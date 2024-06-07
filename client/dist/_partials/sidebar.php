2a322675a9200f58a15cf69ca4b2defea3e0babb
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2024-06-07 16:10:12 +0530

    Accounts is updated with the correct spelling

--- client/dist/_partials/sidebar.php
+++ client/dist/_partials/sidebar.php
@@ -80,7 +80,7 @@
               <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-briefcase"></i>
                 <p>
-                  SRMISTBank Accounts
+                  Accounts
                   <i class="fas fa-angle-left right"></i>
                 </p>
               </a>
