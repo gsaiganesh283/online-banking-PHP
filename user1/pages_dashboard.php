d42bbbffd37f9c45bb272c9759ce39f4d8d34c6c
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2025-01-10 16:32:57 +0530

    number format is done for displaying the output in user dashboard

--- user1/pages_dashboard.php
+++ user1/pages_dashboard.php
@@ -191,7 +191,7 @@ $stmt->close();
                 <div class="info-box-content">
                   <span class="info-box-text"> Deposits</span>
                   <span class="info-box-number">
-                    $ <?php echo $iB_deposits; ?>
+                  ₹ <?php echo number_format($iB_deposits,2); ?>
                   </span>
                 </div>
               </div>
@@ -205,7 +205,7 @@ $stmt->close();
 
                 <div class="info-box-content">
                   <span class="info-box-text">Withdrawals</span>
-                  <span class="info-box-number">$ <?php echo $iB_withdrawal; ?> </span>
+                  <span class="info-box-number">₹ <?php echo number_format($iB_withdrawal,2); ?> </span>
                 </div>
               </div>
             </div>
@@ -220,7 +220,7 @@ $stmt->close();
                 <span class="info-box-icon bg-success elevation-1"><i class="fas fa-random"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Transfers</span>
-                  <span class="info-box-number">$ <?php echo $iB_Transfers; ?></span>
+                  <span class="info-box-number">₹ <?php echo number_format($iB_Transfers,2); ?></span>
                 </div>
               </div>
             </div>
@@ -232,7 +232,7 @@ $stmt->close();
                 <span class="info-box-icon bg-purple elevation-1"><i class="fas fa-money-bill-alt"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Wallet Balance</span>
-                  <span class="info-box-number">$ <?php echo $TotalBalInAccount; ?></span>
+                  <span class="info-box-number">₹ <?php echo number_format($TotalBalInAccount,2); ?></span>
                 </div>
               </div>
             </div>
@@ -280,7 +280,7 @@ $stmt->close();
                   <div class="row">
                     <div class="col-sm-3 col-6">
                       <div class="description-block border-right">
-                        <h5 class="description-header">$ <?php echo $iB_deposits; ?></h5>
+                        <h5 class="description-header">₹ <?php echo number_format($iB_deposits,2); ?></h5>
                         <span class="description-text">TOTAL DEPOSITS</span>
                       </div>
                       <!-- /.description-block -->
@@ -288,7 +288,7 @@ $stmt->close();
                     <!-- /.col -->
                     <div class="col-sm-3 col-6">
                       <div class="description-block border-right">
-                        <h5 class="description-header">$ <?php echo $iB_withdrawal; ?></h5>
+                        <h5 class="description-header">₹ <?php echo number_format($iB_withdrawal,2); ?></h5>
                         <span class="description-text">TOTAL WITHDRAWALS</span>
                       </div>
                       <!-- /.description-block -->
@@ -296,7 +296,7 @@ $stmt->close();
                     <!-- /.col -->
                     <div class="col-sm-3 col-6">
                       <div class="description-block border-right">
-                        <h5 class="description-header">$ <?php echo $iB_Transfers; ?> </h5>
+                        <h5 class="description-header">₹ <?php echo number_format($iB_Transfers,2); ?> </h5>
                         <span class="description-text">TOTAL TRANSFERS</span>
                       </div>
                       <!-- /.description-block -->
@@ -304,7 +304,7 @@ $stmt->close();
                     <!-- /.col -->
                     <div class="col-sm-3 col-6">
                       <div class="description-block">
-                        <h5 class="description-header">$ <?php echo $TotalBalInAccount; ?> </h5>
+                        <h5 class="description-header">₹ <?php echo number_format($TotalBalInAccount,2); ?> </h5>
                         <span class="description-text">TOTAL MONEY IN iBANK Account</span>
                       </div>
                       <!-- /.description-block -->
@@ -378,7 +378,7 @@ $stmt->close();
                             <td><?php echo $row->tr_code; ?></a></td>
                             <td><?php echo $row->account_number; ?></td>
                             <td><?php echo $alertClass; ?></td>
-                            <td>$ <?php echo $row->transaction_amt; ?></td>
+                            <td>₹ <?php echo number_format($row->transaction_amt,2); ?></td>
                             <td><?php echo $row->client_name; ?></td>
                             <td><?php echo date("d-M-Y h:m:s ", strtotime($transTstamp)); ?></td>
                           </tr>
